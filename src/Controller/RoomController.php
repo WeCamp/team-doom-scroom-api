92ea28e40b9b5e632720c448b5d1fea32bd62597
Daniëlle Suurlant <danielle.suurlant@gmail.com>
2018-08-23 12:31:52 +0200

    Create and get room

---
+++ src/Controller/RoomController.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Scroom\Api\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @author Daniëlle Suurlant <danielle@connectholland.nl>
+ */
+class RoomController
+{
+    public function post()
+    {
+        return new Response();
+    }
+}
